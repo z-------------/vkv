254e67e2e0c6d32474d195cb1db6d460ea9dccea
Zack Guard <5311230+z-------------@users.noreply.github.com>
2024-12-22 15:16:53 +0800

    Add test for custom dumpHook

--- tests/tgenerating.nim
+++ tests/tgenerating.nim
@@ -6,6 +6,12 @@ import std/[
   paths,
 ]
 
+proc dumpHook(s: var string; v: Path; depth = 0; topLevel: static bool = false) =
+  dumpHook(s, string v, depth, topLevel)
+
+proc dumpHook(s: var string; v: int; depth = 0; topLevel: static bool = false) =
+  dumpHook(s, $v, depth, topLevel)
+
 test "generate addonlist":
   type
     Root = object
@@ -16,6 +22,21 @@ test "generate addonlist":
       "foo.vpk": "1",
       "bar baz.vpk": "0",
       "foo\\bar": "1",
+    }.toOrderedTable,
+  )
+  let s = root.toKeyvalues
+  check s == readTestFile(Path "expected_addoninfo.txt")
+
+test "custom dumpHook":
+  type
+    Root = object
+      AddonInfo: OrderedTable[Path, int]
+
+  let root = Root(
+    AddonInfo: {
+      Path "foo.vpk": 1,
+      Path "bar baz.vpk": 0,
+      Path "foo\\bar": 1,
     }.toOrderedTable,
   )
   let s = root.toKeyvalues
