40886ae1a0896eb4730e101c9f874818d09a997f
Zack Guard <5311230+z-------------@users.noreply.github.com>
2025-01-23 22:11:20 +0800

    Remove dumpHookArrayImpl

--- vkv/generating.nim
+++ vkv/generating.nim
@@ -60,24 +60,6 @@ type SomeTable[K, V] = Table[K, V] or OrderedTable[K, V]
 proc dumpHook*[K, V](s: var string; v: SomeTable[K, V]; depth: int; topLevel: static bool) =
   dumpHookTableImpl(s, depth, topLevel, pairs(v), false)
 
-template dumpHookArrayImpl(s, depth, topLevel: untyped; iter: iterable) =
-  # similar to the one for tables/objects
-  mixin dumpHook
-  when not topLevel:
-    s.addNewline
-    s.add '\t'.repeat(depth - 1)
-    s.add "{\n"
-  let indent {.inject.} = '\t'.repeat(depth)
-  for idx, val in iter:
-    s.add indent
-    dumpHook(s, $idx, depth + 1, false)
-    s.add '\t'
-    dumpHook(s, val, depth + 1, false)
-    s.add '\n'
-  when not topLevel:
-    s.add '\t'.repeat(depth - 1)
-    s.add "}"
-
 iterator jArrayPairs(v: JsonNode): (int, JsonNode) =
   assert v.kind == JArray
   var idx = 0
@@ -101,10 +83,10 @@ proc dumpHook*(s: var string; v: JsonNode; depth: int; topLevel: static bool) =
   of JObject:
     dumpHookTableImpl(s, depth, topLevel, pairs(v), false)
   of JArray:
-    dumpHookArrayImpl(s, depth, topLevel, jArrayPairs(v))
+    dumpHookTableImpl(s, depth, topLevel, jArrayPairs(v), false)
 
 proc dumpHook*[T](s: var string; v: openArray[T]; depth: int; topLevel: static bool) =
-  dumpHookArrayImpl(s, depth, topLevel, pairs(v))
+  dumpHookTableImpl(s, depth, topLevel, pairs(v), false)
 
 proc dumpHook*[T: object](s: var string; v: T; depth: int; topLevel: static bool) =
   dumpHookTableImpl(s, depth, topLevel, fieldPairs(v), true)
